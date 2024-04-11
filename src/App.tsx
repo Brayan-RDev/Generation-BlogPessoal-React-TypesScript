f720f55d27785453fddd9f71d868988e6b649e1b
Brayan <codinomekillerkira@gmail.com>
2024-04-11 18:50:51 -0300

    Criando componentes NavBar, Home, Footer e Login e criando as rotas entre login e home

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import { Home } from './pages/home'
+import { Navbar } from './components/navBar/NavBar';
+import { Footer } from './components/footer/Footer';
+import { Home } from './pages/home/Home';
+import { Login } from './pages/login/Login';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 export function App() {
   return (
-    <>
-      <Home title='Componente Home' description='Este é um componente que recebe props.'/>
-    </>
-  )
+    <BrowserRouter>
+      <Navbar />
+        <div className='min-h-[80vh]'>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </div>
+        <Footer />
+    </BrowserRouter>
+  );
 }
-
